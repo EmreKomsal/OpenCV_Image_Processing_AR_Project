f4d857ba260f85483b0d08a7efe183227eea400a
EmreKomsal <komsal16@itu.edu.tr>
2021-09-15 16:44:32 +0300

    Image Read Test

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import cv2  # openCV python package
-import numpy as np #numpy package
+import numpy as np  # numpy package
 
 print("Hello OpenCV")
+shape = (640, 480)
+image_matrix = np.ones_like(shape)
+filepath = "Test Image.jpg"
+image_create = cv2.imwrite(filepath, image_matrix)
+image = cv2.imread(filepath)
+cv2.imshow("Test  Image", image)
+cv2.waitKey(0)
