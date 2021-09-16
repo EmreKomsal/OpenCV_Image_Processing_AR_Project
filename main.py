c4be01f86f7c83d0ba9f49dc9963c3d27cb8ec7f
EmreKomsal <komsal16@itu.edu.tr>
2021-09-16 17:45:32 +0300

    Showing images in one window

--- main.py
+++ main.py
@@ -27,11 +27,12 @@ def get_webcam(mirror: bool = False, webcamID: int = 0):
         # Create Canny and Blur Filters
         img_blur, img_canny = Blur_and_Canny_Ege_Image(img, 7, (minValue, maxValue))
         # Stacking images in one window
-        img_stack = np.hstack((img, img_grayScale))
-        img_stack2 = np.hstack((img_blur, img_canny))
+        img_list = [[img, img_grayScale], [img_blur, img_canny]]
+        img_stack = stackImages(0.5, img_list)
+        # img_stack2 = np.hstack((img_blur, img_canny))
         # show video cam stream as a frame by frame image
         cv2.imshow("Webcam", img_stack)
-        cv2.imshow("Webcam_Processed", img_stack2)
+        # cv2.imshow("Webcam_Processed", img_stack2)
         # if ESC key pressed end video capture from webcam
         if cv2.waitKey(1) & 0xFF == 27:
             break
@@ -40,9 +41,36 @@ def get_webcam(mirror: bool = False, webcamID: int = 0):
 
 
 # Find most fitting method for video types
-def Image_Window_Optimizer(img_list):
-    # Find array length for appropriate fit
-    array_length = len(img_list)
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
 
 
 
@@ -56,12 +84,12 @@ def GrayScale_Image_3_Channel(img):
 
 
 # Make blurred and cannied versions of image
-# First Canny Paramaters for Max value, second value for Min value
+# First Canny Parameters for Max value, second value for Min value
 def Blur_and_Canny_Ege_Image(img, matrix_size: int, canny_parameters: (int, int)):
     # Creating gray scale version of image for Gaussian Blur
     img_grayScale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     kernel_matrix = (matrix_size, matrix_size)
-    # Applying Gaussuan Blur to image
+    # Applying Gaussian Blur to image
     img_blur = cv2.GaussianBlur(img_grayScale, kernel_matrix, 0)
     # Finding Canny Edges of image
     img_canny = cv2.Canny(img_blur, canny_parameters[0], canny_parameters[1])
@@ -69,7 +97,7 @@ def Blur_and_Canny_Ege_Image(img, matrix_size: int, canny_parameters: (int, int)
 
 
 def main():
-    get_webcam()
+    get_webcam(mirror= True)
 
 
 if __name__ == '__main__':
